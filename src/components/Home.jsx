6d436d3bc8ab04f669f9859ce9a274213b3bd8f3
Alvin <keringalvin@gmail.com>
2024-08-13 12:41:49 +0300

    Displays bres on web

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import BrewDisplay from './BrewDisplay'
 
 const Home = () => {
+
+// const url = `https://api.openbrewerydb.org/breweries`
+
+const [breweries, setBreweries]=useState([])
+
+useEffect(()=>{
+  fetch('https://api.openbrewerydb.org/breweries')
+  .then(res =>res.json())
+  .then(breweries => setBreweries(breweries))
+},[])
+
+const display = breweries.map(
+  brew => <BrewDisplay brew={brew} key={brew.id} setBreweries={setBreweries}/>
+)
   return (
-    <div>Home</div>
+    <div>
+      {display}
+    </div>
   )
 }
 
