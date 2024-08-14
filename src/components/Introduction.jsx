942fd25e08c408272efa296bd90fccdd86a8cb2c
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-15 02:21:10 +0300

    initial styles to introduction

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import introPhoto from '../assets/introphoto.jpeg';
+import '../styles/Introduction.css'
 
 const Introduction = () => {
   return (
-    <div>
+    <div className='cover'>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ea vero doloremque quia asperiores. Quam fuga explicabo et amet, animi iure earum sed asperiores, minus at sapiente mollitia ipsum ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quas at omnis amet laboriosam? Incidunt, corrupti! Repellendus eveniet dolor ducimus odio, veniam, minima ullam libero, eos harum blanditiis amet nesciunt?</p>
     </div>
   )
