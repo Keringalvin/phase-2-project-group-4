e3e5684984724e2b2e5fca98efe7ee9c2bf1bbcf
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-16 01:51:55 +0300

    Adds introduction styles

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -3,9 +3,15 @@ import '../styles/Introduction.css'
 
 const Introduction = () => {
   return (
+    <>
     <div className='cover'>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ea vero doloremque quia asperiores. Quam fuga explicabo et amet, animi iure earum sed asperiores, minus at sapiente mollitia ipsum ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quas at omnis amet laboriosam? Incidunt, corrupti! Repellendus eveniet dolor ducimus odio, veniam, minima ullam libero, eos harum blanditiis amet nesciunt?</p>
+      <img src='https://images.unsplash.com/photo-1462664450306-25ad625a342b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
     </div>
+    <div className='intro'>
+      <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia perferendis sunt, rerum in veritatis praesentium. Cumque quod a repudiandae! Est, consequuntur asperiores atque quidem quaerat totam nihil architecto tempore voluptate.</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ea vero doloremque quia asperiores. Quam fuga explicabo et amet, animi iure earum sed asperiores, minus at sapiente mollitia ipsum ea! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quas at omnis amet laboriosam? Incidunt, corrupti! Repellendus eveniet dolor ducimus odio, veniam, minima ullam libero, eos harum blanditiis amet nesciunt?</p>
+    </div>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../styles/Navbar.css'
+import '../styles/Navbar.css';
 
 function Navbar() {
   return (
     <div className="nav">
-      <header className="App">Breweries Advertisement</header>
-      <div className="search">
-        <nav className="navbar">
-          <div className="navbar-brand">
-            <Link to="/">My App</Link>
-          </div>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link to="/home">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/form">Form</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <header className="header">Breweries Advertisement</header>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link to="/">My App</Link>
+        </div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/home">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/form">Form</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
