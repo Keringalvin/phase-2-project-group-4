a7eb34712f14942479b4b6029ef7d6e96bef5f50
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-15 23:42:34 +0300

    Enables overflow of brewery data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,7 +34,7 @@ const display = filteringBrands.map(
   return (
     <div className='home-container'>
       <img className='' src='https://images.unsplash.com/photo-1584225065152-4a1454aa3d4e?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
-      {/* <caption>Hi there</caption> */}
+      {/* <caption style={{color: `white`}}>Hi there</caption> */}
       <div className='words'>
         <p style={{color: `white`}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci nobis minus cumque illo voluptates aliquam provident a. Libero impedit eius labore praesentium nulla, eveniet est tempore eligendi delectus maxime incidunt!</p>
       </div>
@@ -43,9 +43,8 @@ const display = filteringBrands.map(
           Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo veniam, rem, maxime temporibus autem ex dolorem dolorum provident totam sapiente est mollitia nisi! Hic aperiam aut odit ipsa in perferendis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab iusto a inventore deserunt aperiam temporibus laudantium esse possimus, voluptates odio fuga, obcaecati, veniam voluptatem nesciunt quae molestias repellendus odit totam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dicta cumque suscipit assumenda perspiciatis nam provident impedit deserunt necessitatibus blanditiis? Totam facilis perferendis at veritatis quis nesciunt numquam veniam. Recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nesciunt non ab fuga, quod deleniti perferendis dolore natus distinctio, commodi magni hic maxime porro recusandae libero debitis, doloremque animi molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, esse facilis. Iste amet recusandae, non earum veritatis quod iure, minus voluptatibus neque molestiae incidunt sit corrupti, dignissimos quidem voluptatum alias.
         </p>
       </div>
-      
-      <div className='fetcher'>
-      <div className='search'>
+      <div>
+      <div className='search-bar'>
       <input 
       type='text'
       placeholder='Search Brand'
@@ -53,7 +52,10 @@ const display = filteringBrands.map(
       onChange={handlesSearch}
       />
       </div>
+      <div className='fetcher'>
+
       {display}
+      </div>
       </div>
     </div>
   )
